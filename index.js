8f5a8c95394e1a3bc0f8fc8655d917e4fcc02bf3
Jana Malecka <jm.jmalecka@gmail.com>
2021-04-20 21:31:09 +0200

    finalni verze s animaci

--- index.js
+++ index.js
@@ -5,20 +5,21 @@ console.log('funguju');
 let move = 'circle';
 const boardFieldElms = document.querySelectorAll('.board__field');
 
-const circleElm = document.querySelector('.circle');
-const crossElm = document.querySelector('.cross');
+const whoPlaysElm = document.querySelector('.who-plays');
 
 const selectBoardField = (event) => {
   if (move === 'circle') {
     event.target.classList.add('board__field--circle');
     /* event.target.innerHTML = `<img src="circle.svg" alt="kolečko" class="circle" />`; //takto bila barva*/
     move = 'cross';
+    whoPlaysElm.src = 'cross.svg';
+    event.target.disabled = true;
   } else {
     event.target.classList.add('board__field--cross');
     move = 'circle';
+    whoPlaysElm.src = 'circle.svg';
+    event.target.disabled = true;
   }
-  event.target.removeEventListener('click', selectBoardField);
-  event.target.disabled = true;
 };
 
 for (let i = 0; i < boardFieldElms.length; i++) {
